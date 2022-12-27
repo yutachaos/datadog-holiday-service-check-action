0225f14acb7e77369237b18c4e6d089cbede0a76
yutachaos <18604471+yutachaos@users.noreply.github.com>
2022-12-28 00:15:32 +0900

    Added datadog service check action

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,31 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {createEvent} from './datadog'
+import {isHoliday} from './holiday'
+
+import {
+  CRITICAL,
+  OK
+} from '@datadog/datadog-api-client/dist/packages/datadog-api-client-v1/models/ServiceCheckStatus'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const holidays: string = core.getInput('holidays')
+    const serviceName: string = core.getInput('holidays')
+    const host: string = core.getInput('datadog_host')
+    const tags: string = core.getInput('datadog_tags')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    process.env['DD_API_KEY'] = core.getInput('datadog_api_key')
+    process.env['DD_APP_KEY'] = core.getInput('datadog_app_key')
+    const result = isHoliday(new Date(), holidays)
+    if (result) {
+      createEvent(CRITICAL, serviceName, host, tags)
+    } else {
+      createEvent(OK, serviceName, host, tags)
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    createEvent(OK, serviceName, host, tags)
+
+    core.setOutput('is_holiday', result)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
